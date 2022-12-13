608259874ebd2c55631253073a1173e0781401dd
TimothyGakunju <gakunjug@gmail.com>
2022-12-13 22:13:33 +0300

    Added script file

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,15 @@
 $(document).ready(function () {
-  //search button feature
+  
   $("#search-button").on("click", function () {
-    //get value in input search-value.
+    
     var searchTerm = $("#search-value").val();
-    //empty input field.
+    
     $("#search-value").val("");
     weatherFunction(searchTerm);
     weatherForecast(searchTerm);
   });
 
-  //search button enter key feature. 
+  
   $("#search-button").keypress(function (event) {
     var keycode = (event.keyCode ? event.keyCode : event.which);
     if (keycode === 13) {
@@ -18,25 +18,25 @@ $(document).ready(function () {
     }
   });
 
-  //pull previous searches from local storage
+  
   var history = JSON.parse(localStorage.getItem("history")) || [];
 
-  //sets history array search to correct length
+  
   if (history.length > 0) {
     weatherFunction(history[history.length - 1]);
   }
-  //makes a row for each element in history array(searchTerms)
+  
   for (var i = 0; i < history.length; i++) {
     createRow(history[i]);
   }
 
-  //puts the searched cities underneath the previous searched city 
+  
   function createRow(text) {
     var listItem = $("<li>").addClass("list-group-item").text(text);
     $(".history").append(listItem);
   }
 
-  //listener for list item on click function
+  
   $(".history").on("click", "li", function () {
     weatherFunction($(this).text());
     weatherForecast($(this).text());
@@ -50,15 +50,15 @@ $(document).ready(function () {
 
 
     }).then(function (data) {
-      //if index of search value does not exist
+      
       if (history.indexOf(searchTerm) === -1) {
-        //push searchValue to history array
+        
         history.push(searchTerm);
-        //places item pushed into local storage
+       
         localStorage.setItem("history", JSON.stringify(history));
         createRow(searchTerm);
       }
-      // clears out old content
+      
       $("#today").empty();
 
       var title = $("<h3>").addClass("card-title").text(data.name + " (" + new Date().toLocaleDateString() + ")");
@@ -101,7 +101,7 @@ $(document).ready(function () {
 
       });
 
-      // merge and add to page
+      
       title.append(img);
       cardBody.append(title, temp, humid, wind);
       card.append(cardBody);
@@ -109,7 +109,7 @@ $(document).ready(function () {
       console.log(data);
     });
   }
-  // function weatherForecast(searchTerm) 
+  
   function weatherForecast(searchTerm) {
     $.ajax({
       type: "GET",
@@ -119,7 +119,7 @@ $(document).ready(function () {
       console.log(data);
       $("#forecast").html("<h4 class=\"mt-3\">5-Day Forecast:</h4>").append("<div class=\"row\">");
 
-      //loop to create a new card for 5 days pull data image from search
+      
       for (var i = 0; i < data.list.length; i++) {
 
         if (data.list[i].dt_txt.indexOf("15:00:00") !== -1) {
@@ -132,9 +132,9 @@ $(document).ready(function () {
           var humidFive = $("<p>").addClass("card-text").text("Humidity: " + data.list[i].main.humidity + "%");
           var tempFive = $("<p>").addClass("card-text").text("Temperature: " + data.list[i].main.temp + " °F");
 
-          //merge together and put on page
+          
           colFive.append(cardFive.append(cardBodyFive.append(titleFive, imgFive, tempFive, humidFive)));
-          //append card to column, body to card, and other elements to body
+          
           $("#forecast .row").append(colFive);
         }
       }
